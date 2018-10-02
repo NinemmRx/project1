c11180b786bd3d4d86af1109f0245f1db024e32d
jstampfli <jakestampfli@mac.com>
2018-10-01 19:56:22 -0700

    version2

--- restaurant.js
+++ restaurant.js
@@ -1,40 +1,109 @@
-$(document).ready(function() {
+// https://sushi-hunt.herokuapp.com/
 
-    $("button").on("click", function() {
+// use this link: https://developers.zomato.com/api/v2.1/location_details?entity_id=302&entity_type=city
+
+
+$(document).ready(function () {
+  $( window ).on( "load", function() {
     
-      var word = $("#recipes").val();
-      console.log(word ,"esta es la palabra");
+    $("#cuisine").val("Mexican");
+    console.log("#cuisine");
+    findRestaurants();
+  })
   
-    // var queryURL = "https://www.food2fork.com/api/get?key=1da509da23ed2e8894b619e4e4dc25bb&rId=35382";
-    //var queryURL = "https://www.food2fork.com/api/search?key=1da509da23ed2e8894b619e4e4dc25bb&q="+ word +"&page=2";
-    var queryURL = "https://developers.zomato.com/api/v2.1/locations?query=San%20diego"
-    
-    $.ajax({  
-        url: "https://developers.zomato.com/api/v2.1/locations?query=San%20diego",
-        dataType: 'json',
-        async: true,
-        beforeSend: function(xhr){xhr.setRequestHeader('user-key', 
-        '4293f75ed134cc3dc7f4f497a4a7a0ae');},  // This inserts the api key into the HTTP header
-        success: function(response) { console.log(response) } });
+  $("#submit").on("click", function (event) {
+    console.log("submit clicked")
+    event.preventDefault();
+   $("#tbody").empty();
+    findRestaurants();
+  });
 
-    //     headers:{
-    //     'user_key': '4293f75ed134cc3dc7f4f497a4a7a0ae',
-    //     },
-    //   url: queryURL,
-    //   method: "GET",
-    //   dataType:"json",
-    //   //data: word,
-    //   success: function(response) {
-    //     console.log(response);
-    //     // Saving the image_original_url property
-    //     // var result = response.recipes;
-    //     // var recipeDiv = $("<div>");
-    //     // recipeDiv.attr("data", result);
-    //     // $("#search").html(`<p>${JSON.stringify(result)}</p>`);
+  // find restaurants 
+
+
+  function findRestaurants() {
+
+    var cuisine = $("#cuisine").val().trim();
+    // var cuisine = $("#sel1");
+
+
+    // =302 - city code 216 + country code
+    var queryURL = "https://developers.zomato.com/api/v2.1/search?entity_id=302&entity_type=city&q=" + cuisine;
+
+
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+      headers: { "user-key": "4293f75ed134cc3dc7f4f497a4a7a0ae" },
+      dataType: "json",
+    })
+
+      // API response
+      .then(function (response) {
+        
+        console.log(response);
+        var restaurantData = response.restaurants
+        
+       //var urlText = $("#url").attr("src", restaurantData[i].restaurant.events_url);
+       // var urlAnchor = $("<a>").attr("href", restaurantData[i].restaurant.events_url.html).html("Website");
+
+        for (var i = 0; i < restaurantData.length; i++) { 
          
+    
+        var tableRow = $("<tr>");
+        tableRow.append($("<td>").text(restaurantData[i].restaurant.name));
+        tableRow.append($("<td>").text(restaurantData[i].restaurant.location.address));
+        
+        tableRow.append($("<td>").html("<a target='_blank' href='"+restaurantData[i].restaurant.events_url+"'>Visit</a>"));
+
+        // tableRow.append($("<td>").html(urlAnchor));
+                  //     ($("<a>","#url").attr(, restaurantData[i].restaurant.events_url);
+                       
+
+
+        tableRow.append($("<td>").text(restaurantData[i].restaurant.user_rating.rating_text));
+        // tableRow.append($("<td>").text(restaurantData[i].restaurant.price_range));
+
+        $("#tbody").append(tableRow);
+        
+      }
+      $("#cuisine").val("").trim();
       
 
-    
-  })
-  })
-  
+
+
+
+
+
+        // Get API TO PRINT
+        database.reg().on("child_added", function (snapshot) {
+          //var to store input data -- maybe later define limit to display
+          
+          
+          
+          
+          
+          /*{ FIRE BASE USE LATER create VAR
+            name: snapshot.val().name,
+            address: snapshot.val().address,
+            cuisines: snapshot.val().cuisines,
+            events_url: snapshot.val().events_url,
+            rating_text: snapshot.val().rating_text,
+            price_range: snapshot.val().price_range,
+          }; */
+
+        /*  var tableRow = $("<tr>");
+          tableRow.append($("<td>").text(restaurantData.name));
+          tableRow.append($("<td>").text(restaurantData.address));
+          tableRow.append($("<td>").text(restaurantData.cuisines));
+          tableRow.append($("<td>").text(restaurantData.events_url));
+          tableRow.append($("<td>").text(restaurantData.events_url));
+          tableRow.append($("<td>").text(restaurantData.events_url));
+
+          $("#tbody").append(tableRow); */
+
+        });
+      });
+
+  };
+});
